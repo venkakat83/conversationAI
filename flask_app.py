b5dd119bfa91d0cf4bf6d5f13e9027bcb9ec89c3
venkakat83 <k.venkataraju@gmail.com>
2024-09-14 12:46:07 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
+@app.route("/")
+def home():
+    return "Hello, World!"
 
-# Define a route for recognizing named entities
-@app.route('/named_entity', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    data['sentence'] = data['sentence'].replace("\n", " ")
-    data_updated = {data['sentence']}
-    return jsonify({'named_entities': "Venkat"})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
